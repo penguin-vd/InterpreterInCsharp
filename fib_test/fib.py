418a254b9c763eaab4e2db70b90c5b6e80f1f489
Levi van Daalen <2004levi@gmail.com>
2023-06-09 22:30:38 +0200

    created a way to test compare bigl to python in performance!

---
+++ fib_test/fib.py
@@ -0,0 +1,15 @@
+import time
+
+def fib(n):
+    a = 0
+    b = 1
+    for _ in range(0, n):
+        (a, b) = (b, a + b)
+    return a
+
+def run(f, i):
+    t0 = time.time(); f(i); t1 = time.time()
+    print((t1 - t0) * 1000)
+
+run(fib, 10000000)
+
